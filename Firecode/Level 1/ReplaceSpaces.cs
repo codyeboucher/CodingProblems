60ee298978316d77d53c42f7cb3aa59e9d993234
Cody <codyeboucher@gmail.com>
2019-05-08 22:38:08 -0500

    ReplaceSpaces
    Replace spaces in string with given replacement string without string.Replace using StringBuilder.

---
+++ Firecode/Level 1/ReplaceSpaces.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelOne
+{
+    class ReplaceSpaces
+    {
+        /* Write a method to replace all spaces in a string with a given replacement string. 
+        * replace("This is a test","/") --> "This/is/a/test"
+        * Note: Avoid using the in-built String.Replace() method. */
+
+        public static string Replace(string input, string replacement)
+        {
+            var sb = new StringBuilder();
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                {
+                    sb.Append(input[i]);
+                }
+                else
+                {
+                    sb.Append(replacement);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
