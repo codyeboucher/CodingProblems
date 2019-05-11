4d00701901592d65c4764981c835cdc199853252
Cody <codyeboucher@gmail.com>
2019-05-11 18:40:27 -0500

    First Non Duplicate Character in a String
    Finds the first non duplicate char in a string, uses dictionary to store chars and their count, return null if no duplicate.

---
+++ Firecode/Level 1/FirstNonDuplicateChar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelOne
+{
+    class FirstNonDuplicateChar
+    {
+        /*Find the first non-duplicate character in a string. Return null if no unique character is found. 
+        * firstNonRepeatedCharacter( "abcdcd" ) --> 'a'
+        * firstNonRepeatedCharacter( "cbcd" ) --> 'b'
+        * firstNonRepeatedCharacter( "cdcd" ) --> null */
+
+        public static char? FirstNonDupChar(string input)
+        {
+            var charCount = new Dictionary<char, int>();
+            foreach (var ch in input)
+            {
+                if(charCount.ContainsKey(ch))
+                {
+                    charCount[ch]++;
+                }
+                else
+                {
+                    charCount.Add(ch, 1);
+                }
+            }
+
+            foreach (var ch in input)
+            {
+                if(charCount[ch] == 1)
+                {
+                    return ch;
+                }
+            }
+            return null;
+        }
+    }
+}
