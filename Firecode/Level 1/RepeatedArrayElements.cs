64313127eafdbb9215d94070e6aefbc4c8f6de9f
Cody <codyeboucher@gmail.com>
2019-05-11 21:19:48 -0500

    Repeated Elements in an Array
    Returns the duplicate numbers in an array in ascending order

---
+++ Firecode/Level 1/RepeatedArrayElements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelOne
+{
+    class RepeatedArrayElements
+    {
+        /*Write a method duplicate to find the repeated or duplicate elements in an array. 
+        * This method should return a list of repeated integers in a string with the elements sorted in ascending order 
+        * (as illustrated below). 
+        * duplicate({1,3,4,2,1}) --> "[1]"
+        * duplicate({1,3,4,2,1,2,4}) --> "[1, 2, 4]"*/
+
+        public static string Duplicate(int[] numbers)
+        {
+            var nums = new HashSet<int>();
+            var duplicates = new SortedSet<int>();
+
+            foreach (var num in numbers)
+            {
+                if(nums.Contains(num))
+                {
+                    duplicates.Add(num);
+                }
+                else
+                {
+                    nums.Add(num);
+                }
+            }
+            return string.Join(" ", duplicates);
+        }
+    }
+}
