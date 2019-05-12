d3a07914a81744cfd43d16a9d8603b164be84e8c
Cody <codyeboucher@gmail.com>
2019-05-12 13:22:53 -0500

    Missing Number
    Finds the missing number in an array from 1 - 10

---
+++ Firecode/Level 1/MissingNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelOne
+{
+    class MissingNumber
+    {
+        /*Given an Array containing 9 numbers ranging from 1 to 10, write a method to find the missing number. 
+        * Assume you have 9 numbers between 1 to 10 and only one number is missing.
+        * findMissingNumber({1,2,4,5,6,7,8,9,10}) --> 3*/
+
+        public static int FindMissingNum(int[] nums)
+        {
+            var total = 55;
+            foreach (var num in nums)
+            {
+                total -= num;
+            }
+            return total;
+        }
+    }
+}
