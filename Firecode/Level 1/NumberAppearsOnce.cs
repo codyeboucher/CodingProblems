585dd1dd74982a5ab84f47709753a618163e492f
Cody <codyeboucher@gmail.com>
2019-05-13 22:00:16 -0500

    Find Number That Appears Once
    Returns the number that appears only once in the array

---
+++ Firecode/Level 1/NumberAppearsOnce.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelOne
+{
+    class NumberAppearsOnce
+    {
+        /*Write a method that returns a number that appears only once in an array.
+        * Assume the array will surely have a unique value. The array will never be empty.
+        * Examples:
+        * {1,2,3,4,1,2,4,3,5} ==> 5*/
+
+        public static int SoloNumber(int[] nums)
+        {
+            var result = 0;
+            var numberCount = new Dictionary<int, int>();
+
+            foreach (var num in nums)
+            {
+                if(numberCount.ContainsKey(num))
+                {
+                    numberCount[num]++;
+                }
+                else
+                {
+                    numberCount.Add(num, 1);
+                }
+            }
+
+            foreach (var num in nums)
+            {
+                if(numberCount[num] == 1)
+                {
+                    result = num;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
