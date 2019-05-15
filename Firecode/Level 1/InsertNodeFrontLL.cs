881faf37f7ca1f177f54f063ed866c9db05009af
Cody <codyeboucher@gmail.com>
2019-05-14 20:53:59 -0500

    Insert Node Front of Linked List
    Missing node class but basic implementation to add node at front of a linked list.

---
+++ Firecode/Level 1/InsertNodeFrontLL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelOne
+{
+    class InsertNodeFrontLL
+    {
+        /*Write a method to insert a node at the front of a singly-linked list and return the head of the modified list.
+        * Examples:
+        * LinkedList: 1->2 , Head = 1
+        * InsertAtHead(Head,1) ==> 1->1->2
+        * InsertAtHead(Head,2) ==> 2->1->2
+        * InsertAtHead(Head,3) ==> 3->1->2*/
+
+        //public ListNode InsertAtHad(ListNode head, int data)
+        //{
+        //    var newNode = new ListNode(data);
+
+        //    if(head == null)
+        //    {
+        //        head = newNode;
+        //    }
+        //    else
+        //    {
+        //        newNode.Next = head;
+        //        head = newNode;
+        //    }
+            
+        //    return head;         
+        //}
+    }
+}
