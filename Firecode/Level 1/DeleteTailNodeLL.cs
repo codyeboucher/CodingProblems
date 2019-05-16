85f9b00757f4422429a1b31e0f9f3e0d5251cf2b
Cody <codyeboucher@gmail.com>
2019-05-15 20:16:20 -0500

    Delete Tail Node of Linked List
    Delete the tail node of a singly linked list (assuming tail node is not stored)

---
+++ Firecode/Level 1/DeleteTailNodeLL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelOne
+{
+    class DeleteTailNodeLL
+    {
+        /*Given a singly-linked list, write a method to delete its last node and return the head.
+        * Example:
+        * 1->2->3->4 ==> 1->2->3*/
+
+        public ListNode DeleteTail(ListNode head)
+        {
+            if(head == null || head.next == null)
+            {
+                return null;
+            }
+
+            var curr = head;
+            
+            while(curr.next.next != null)
+            {
+                curr = curr.next;
+            }
+
+            curr.next = null;
+            return head;
+        }
+    }
+}
