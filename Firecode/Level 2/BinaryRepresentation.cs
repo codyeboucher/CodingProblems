6227766553b86c52330651534bb702204dfc16a7
Cody <codyeboucher@gmail.com>
2019-05-20 22:25:47 -0500

    Binary Representation
    Convert int to string representation of its binary value

---
+++ Firecode/Level 2/BinaryRepresentation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTwo
+{
+    class BinaryRepresentation
+    {
+        /*Write a method to compute the binary representation of a positive integer. The method should return a string with 1s and 0s. 
+        * computeBinary(6) ==> "110"
+        * computeBinary(5) ==> "101"
+        * Note: Use the minimum number of binary digits needed for the representation (Truncate unnecessary trailing 0s).
+        * computeBinary(5) ==> "0101" (incorrect)
+        * computeBinary(5) ==> "101" (correct)*/
+
+        public static string ComputeBinary(int num)
+        {
+            var sb = new StringBuilder();
+
+            if(num < 2)
+            {
+                return num.ToString();
+            }
+
+            while(num > 0)
+            {
+                sb.Insert(0, num % 2);
+                num /= 2;
+            }
+            return sb.ToString();
+        }
+    }
+}
