75fc028e68444e536eb240d2f0f076b204f8ed77
Cody <codyeboucher@gmail.com>
2019-05-23 21:28:14 -0500

    Couple Sum
    Finds the two numbers in an array that add up to target.

---
+++ Firecode/Level 2/CoupleSum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTwo
+{
+    class CoupleSum
+    {
+        /*Given an array of integers, find two numbers such that they sum up to a specific target.
+        * The method coupleSum should return the indices of the two numbers in the array, where index1 must be less than index2. 
+        * Please note that the indices are not zero based, and you can assume that each input has exactly one solution. 
+        * Target linear runtime and space complexity.
+        * Example:
+        * Input Array : {2, 3, 4, 7}  
+        * Target      : 7
+        * Output      : {2, 3}*/
+
+        public static int[] FindPair(int[] nums, int target)
+        {
+            var targetPair = new Dictionary<int, int>();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(targetPair.ContainsKey(nums[i]))
+                {
+                    return new int[] { targetPair[nums[i]], i + 1 };
+                }
+                else
+                {
+                    targetPair.Add(target - nums[i], i + 1);
+                }
+            }
+            return null;
+        }
+    }
+}
