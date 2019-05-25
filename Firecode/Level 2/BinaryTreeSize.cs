64a813efd72edc88bf44d3822651593e50d92774
Cody <codyeboucher@gmail.com>
2019-05-24 20:41:37 -0500

    Binary Tree Size
    Returns the node count of a binary tree recursively and iteratively with stack.

---
+++ Firecode/Level 2/BinaryTreeSize.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTwo
+{
+    class BinaryTreeSize
+    {
+        /*Given a binary tree, write a method to return its size. 
+         * The size of a tree is the number of nodes it contains.*/
+        
+        //recursively find tree size
+        public static int TreeSizeRec(TreeNode root)
+        {
+            if(root == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1 + TreeSize(root.left) + TreeSize(root.right);
+            }
+        }
+
+        //iteratively find tree size
+        public static int TreeSizeItr(TreeNode root)
+        {
+            if(root == null)
+            {
+                return 0;
+            }
+
+            var nodeCount = 0;
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while(stack.Any())
+            {
+                var curr = stack.Pop();
+                nodeCount++;
+
+                if(curr.right != null)
+                {
+                    stack.Push(curr.right);
+                }
+                if(curr.left != null)
+                {
+                    stack.Push(curr.left);
+                }
+            }
+
+            return nodeCount;
+
+        }
+    }
+}
