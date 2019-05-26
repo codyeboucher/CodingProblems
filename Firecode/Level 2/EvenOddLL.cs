58bd0a1010e982cebff0078d7ae5b6bc666ed26a
Cody <codyeboucher@gmail.com>
2019-05-25 20:42:23 -0500

    Linked List Even or Odd
    Returns true or false if linked list has even number of elements.

---
+++ Firecode/Level 2/EvenOddLL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTwo
+{
+    class EvenOddLL
+    {
+        /*Given a singly-linked list, check whether its length is even or odd in a single pass. 
+         * An Empty list has 0 nodes which makes the number of nodes in it even.
+        * Examples:
+        * 1->2->3->4 == true 1->2->3->4->5 == false*/
+
+        public static bool IsListEven(ListNode head)
+        {
+            if (head == null)
+            {
+                return true;
+            }
+
+            var curr = head;
+
+            while (curr != null || curr.next != null)
+            {
+                curr = curr.next.next;
+            }
+
+            return curr == null;
+
+        }
+    }
+}
