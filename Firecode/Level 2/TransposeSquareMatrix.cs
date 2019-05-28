cc14975b244d21fea8560bf89145b9fc6c2f6919
Cody <codyeboucher@gmail.com>
2019-05-27 19:41:09 -0500

    Transpose Square Matrix
    Transposes the given matrix without new array

---
+++ Firecode/Level 2/TransposeSquareMatrix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTwo
+{
+    class TransposeSquareMatrix
+    {
+        /*You are given a square 2D image matrix where each integer represents a pixel. 
+        * Write a method transposeMatrix to transform the matrix into its Transpose - in-place. 
+        * Example:Input image :
+        * 1 0
+        * 1 0 
+        * Modified to :
+        * 1 1
+        * 0 0*/
+
+        public static void TransposeMatrix(int[,] matrix)
+        {
+            var a = matrix.GetLength(0);
+            var b = matrix.GetLength(1);
+
+            for(int row = 0; row < a; row++)
+            {
+                for(int col = row + 1; col < b; col++)
+                {
+                    var temp = matrix[row, col];
+                    matrix[row, col] = matrix[col, row];
+                    matrix[col, row] = temp;
+                }
+            }
+        }
+    }
+}
