d3c5c19e91ddb44bb85335a0db4a61a4229483da
Cody <codyeboucher@gmail.com>
2019-06-06 20:59:36 -0500

    Is Power Of Two
    Determines if a number is a power of two using bit manipulation

---
+++ Firecode/Level 2/PowerOfTwo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelTwo
+{
+    class PowerOfTwo
+    {
+        /*Write a method - isPowOfTwo to test whether or not a given positive integer is a power of 2. 
+        * Your method should run in constant O(1) time and use O(1) space.
+        * Examples: 
+        * isPowOfTwo(5) ==> false
+        * isPowOfTwo(8) ==> true*/
+
+        public static bool IsPowerOfTwo(int num)
+        {
+            return (num != 0) && (num & (num - 1)) == 0;
+        }
+    }
+}
